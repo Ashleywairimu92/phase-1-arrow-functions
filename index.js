12145415259a2d670680fe307ecfd66155537e1c
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-06-29 17:03:42 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+let a = 2000;
+let b = 100;
+
+const divide = function (a, b) {
+    return a/b;
+}
+console.log(divide (a,b));
+
+let x = 2
+
+const square = function (x){
+    return x*x;
+}
+console.log(square(x));
+
+let c = 3;
+let d = 4;
+
+const add = function (c, d){
+    return c+d;
+}
+console.log(add(c, d));
